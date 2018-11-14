74f264dda24b0cf155e129a977876d7917b0a800
ash <jhshie@gmail.com>
2018-11-14 14:10:17 +0800

    (hotfix) add docs folder

---
+++ docs/precache-manifest.2edc1b5c4d286398b124513946544fa3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c3e500a99692ead68341",
+    "url": "/static/js/main.c3e500a9.chunk.js"
+  },
+  {
+    "revision": "ace8d208b751c7482694",
+    "url": "/static/js/1.ace8d208.chunk.js"
+  },
+  {
+    "revision": "96e867e977057281dde19a8572de40fe",
+    "url": "/index.html"
+  }
+];
